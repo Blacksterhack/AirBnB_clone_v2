dde1c52712f9cdd05e6631ae6e93b4e1eb8e1a59
Blacksterhack <blastertv1001@gmail.com>
2021-08-26 14:47:26 -0500

    static_web

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,26 +1,15 @@
 #!/usr/bin/env bash
-# Creating server configuration.
+# install nginx and configure it to serve some content
+if ! which nginx > /dev/null 2>&1; then
 sudo apt-get -y update
 sudo apt-get -y install nginx
-
-# Create the required folders
-sudo mkdir -p /data/web_static/releases/test/
+fi
 sudo mkdir -p /data/web_static/shared/
-
-# Create a mock HTML file
-echo "Hello, I'm NGINX!!!" >> /data/web_statict/releases/test/index.html
-
-# Create a Symbolic Link
-sudo ln -sf /data/web_statict/releases/test/ /data/web_statict/current
-
-# Give ownership of the /data/ folder to the ubuntu user AND group
-sudo chown -R ubuntu:ubuntu /data
-
-# Update the NGINX config to serve the content
-sudo sed -i "/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }" /etc/nginx/sites-available/default
-
-# Restart the NGINX server
-sudo service nginx restart
-
-# Exit
-exit 0
+sudo mkdir -p /data/web_static/releases/test/
+sudo echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+myc="\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/\;\n\t}\n"
+st="server {"
+sudo sed -i "s/^$st/$st$myc/" /etc/nginx/sites-enabled/default
+sudo service nginx start
